8b5cf5704a7c14cce8676024f17427080908de5b
arond <aron4733@gmail.com>
2020-04-15 11:10:12 +0200

    parce qu'intelJ c'est chiant.

--- src/test/java/com/scholanova/ecommerce/cart/service/CartServiceImplTest.java
+++ src/test/java/com/scholanova/ecommerce/cart/service/CartServiceImplTest.java
@@ -7,14 +7,16 @@ import com.scholanova.ecommerce.product.entity.Product;
 import com.scholanova.ecommerce.product.repository.ProductRepository;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
-@Disabled
+@ExtendWith(MockitoExtension.class)
 class CartServiceImplTest {
 
     @Mock
@@ -22,12 +24,11 @@ class CartServiceImplTest {
     @InjectMocks
     private CartServiceImpl service;
 
-    public CartServiceImplTest(CartServiceImpl service) {
+    /*public CartServiceImplTest(CartServiceImpl service) {
         this.service = service;
-    }
+    }*/
 
     @Test
-    @Disabled
     public void addProductToCart_ShouldAddTheProductToTheCart() {
         //given
         Cart cart = new Cart();
