6ed179206cb14e422c9a0f005eddfbbe37df2875
arond <aron4733@gmail.com>
2020-04-15 11:10:32 +0200

    implémentation addproduct

--- src/main/java/com/scholanova/ecommerce/cart/service/CartServiceImpl.java
+++ src/main/java/com/scholanova/ecommerce/cart/service/CartServiceImpl.java
@@ -1,15 +1,20 @@
 package com.scholanova.ecommerce.cart.service;
 
 import com.scholanova.ecommerce.cart.entity.Cart;
+import com.scholanova.ecommerce.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CartServiceImpl implements CartService{
 
+    @Autowired
+    ProductRepository repository_product;
+
     @Override
     public Cart addProductToCart(Cart cart, Long productId, int quantity) {
-        //TODO
-        return null;
+        cart.addProduct(repository_product.findById(productId).get(), quantity);
+        return cart;
     }
 
     @Override
